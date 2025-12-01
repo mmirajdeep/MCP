7441e955cb63698ae7730fe6a3b9d6290f3391f2
mmirajdeep <mirajverisk@gmail.com>
2025-12-01 18:39:41 +0545

    Refactor RetrievalTools initialization parameters
    Updated GoogleGenerativeAIEmbeddings and Pinecone initialization parameters for the RetrievalTools class.

--- tools/retrival_tools.py
+++ tools/retrival_tools.py
@@ -9,16 +9,16 @@ class RetrievalTools:
     def __init__(self, mcp: FastMCP):
         self.mcp = mcp
         self.embeddings_model = GoogleGenerativeAIEmbeddings(
-            model=settings.EMBEDDINGS_MODEL,
-            google_api_key=settings.GOOGLE_API_KEY,
+            model="models/gemini-embedding-001",
+            google_api_key="AIzaSyBeU28CBIje8s-XY2ZeVteWu1xr_do5Yw8",
         )
         self.pinecone_client = Pinecone(
-            api_key=settings.PINECONE_API_KEY,
+            api_key="pcsk_3a1KGk_9VmJNrS3y3igZLgyExCSGpzE5Q2PV3uXWywMNkuDFJkrDgyj1C26ATi41jrF15w",
             ssl_verify=False,
         )
-        self.index = self.pinecone_client.Index(settings.INDEX_NAME)
-        self.namespace = settings.NAME_SPACE
-        self.index_name = settings.INDEX_NAME
+        self.index = self.pinecone_client.Index(mcp-index")
+        self.namespace = "nepal_docs"
+        self.index_name = "mcp-index"
         logger.info("Registering RetrievalTools methods...")
         self.register_tools()
     
@@ -59,3 +59,4 @@ class RetrievalTools:
             logger.info(f"Registered tool: {name}")
         logger.info("="*40)
         
+
